4c5401c6c7860a14abba3561303f49c64e8c497c
OlesIllich <116875760+OlesIllich@users.noreply.github.com>
2022-11-17 20:26:47 +0200

    init

---
+++ DotNetLearning/DotNetLearning/KeyWorlds/KeyWordsTask.cs
@@ -0,0 +1,25 @@
+﻿namespace DotNetLearning.KeyWorlds
+{
+    public class KeyWordsTask
+    {
+        public int SetInt(object obj)
+        {
+            return (int)obj;
+        }
+
+        public string SetString(object obj)
+        {
+            return (string)obj;
+        }
+
+        public char SetChar(object obj)
+        {
+            return (char)obj;
+        }
+
+        public double SetDouble(object obj)
+        {
+            return (double)obj;
+        }
+    }
+}
---
+++ DotNetLearning/DotNetLearning/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using DotNetLearning.KeyWorlds;
+
+Console.WriteLine("Hello, World!");
+
+//Write all methods, with right and wrong
+var keyWordsTask = new KeyWordsTask();
+
