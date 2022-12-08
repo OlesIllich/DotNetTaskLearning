7481a97dad90b038fc235727dee3af08dd895ff4
SergiiZInkovskii <zsircoz@gmail.com>
2022-12-08 15:53:03 +0200

    Binary

---
+++ Binary search/Binary search/BinarySearch.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Binary_search
+{
+    internal class BinarySearch
+    {
+        static int Search(int[] array, int searchedValue, int left, int right)
+        {
+
+            while (left <= right)
+            {
+
+                int middle = (left + right) / 2;
+
+                if (searchedValue == array[middle])
+                {
+                    return middle;
+                }
+                else if (searchedValue < array[middle])
+                {
+
+                    right = middle - 1;
+                }
+                else
+                {
+                    left = middle + 1;
+                }
+            }
+            return -1;
+        }
+    }
+}
--- Binary search/Binary search/Program.cs
+++ Binary search/Binary search/Program.cs
@@ -4,34 +4,10 @@ namespace Binary_search
 {
     internal class Program
     {
-        static int BinarySearch(int[] array, int searchedValue, int left, int right)
-        {
-            
-            while (left <= right)
-            {
-                
-                int middle = (left + right) / 2;
-
-                if (searchedValue == array[middle])
-                {
-                    return middle;
-                }
-                else if (searchedValue < array[middle])
-                {
-                    
-                    right = middle - 1;
-                }
-                else
-                {
-                    left = middle + 1;
-                }
-            }
-            return -1;
-        }
-
-
+        
         static void Main(string[] args)
         {
+            BinarySearch search = new BinarySearch();
             Console.WriteLine("*****Let's try binary search******");
             int[] newArray = new int[100000];
 
