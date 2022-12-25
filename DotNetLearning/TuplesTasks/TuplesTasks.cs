9baba40db5d7438c26ecda9eb5fb8bc3981ac503
OlesIllich <116875760+OlesIllich@users.noreply.github.com>
2022-12-25 23:34:04 +0200

    add Tuples tasks

---
+++ DotNetLearning/TuplesTasks/TuplesTasks.cs
@@ -0,0 +1,25 @@
+﻿namespace DotNetLearning.TuplesTasks
+{
+    public class TuplesTasks
+    {
+        public (string String, int Number, bool Bool) GetTuple(string firstLetter, int number, bool boolParam)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string GeStringPart(Tuple<string,int,bool> tuple)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int GetIntPart(Tuple<string, int, bool> tuple)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool GetBoolPart(Tuple<string, int, bool> tuple)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ DotNetLearningTest/TuplesTasksTest.cs
@@ -0,0 +1,69 @@
+﻿using AutoFixture.Xunit2;
+using DotNetLearning.TuplesTasks;
+using FluentAssertions;
+using Xunit;
+
+namespace DotNetLearningTest
+{
+    public class TuplesTasksTest
+    {
+        private readonly TuplesTasks _task;
+
+        public TuplesTasksTest()
+        {
+            _task = new TuplesTasks();
+        }
+
+        [Theory]
+        [AutoData]
+        public void GetTupleTest(string str, int i, bool b)
+        {
+            // Arrange
+            // Act
+            var result = _task.GetTuple(str, i, b);
+
+            // Assert
+            result.String.Should().Be(str);
+            result.Bool.Should().Be(b);
+            result.Number.Should().Be(i);
+        }
+
+
+        [Theory]
+        [AutoData]
+        public void GeStringPartTest(string str, int i, bool b)
+        {
+            // Arrange
+            // Act
+            var result = _task.GeStringPart(new Tuple<string, int, bool>(str, i, b));
+
+            // Assert
+            result.Should().Be(str);
+        }
+
+        [Theory]
+        [AutoData]
+        public void GetIntPartTest(string str, int i, bool b)
+        {
+            // Arrange
+            // Act
+            var result = _task.GetIntPart(new Tuple<string, int, bool>(str, i, b));
+
+            // Assert
+            result.Should().Be(i);
+        }
+
+        [Theory]
+        [AutoData]
+        public void GeBoolPartTest(string str, int i, bool b)
+        {
+            // Arrange
+            // Act
+            var result = _task.GetBoolPart(new Tuple<string, int, bool>(str, i, b));
+
+            // Assert
+            result.Should().Be(b);
+        }
+
+}
+}
