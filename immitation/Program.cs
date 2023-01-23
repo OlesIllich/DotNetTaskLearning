bb2f07ac57758295d43188a5483b5cd25496b31b
SergiiZInkovskii <zsircoz@gmail.com>
2023-01-23 10:41:49 +0200

    tasks 10.01 and 19.01 (2)

---
+++ immitation/Program.cs
@@ -0,0 +1,47 @@
+﻿namespace immitation
+{
+    internal class Program
+    {
+        public class A
+        {
+            public virtual void Method()
+            {
+                Console.WriteLine("A");
+            }
+        }
+
+
+        public class B : A
+        {
+            public virtual new void Method()
+            { 
+                Console.WriteLine("B");
+            }
+        }
+
+        public class C : B
+        {
+            public override void Method()
+            {
+                Console.WriteLine("C");
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            A a = new B();
+            B b = new B();
+            B b2 = new C();
+            A c = new C();
+            a.Method();
+            b.Method(); 
+            b2.Method();
+            c.Method();
+            Console.WriteLine(a.GetType());
+            Console.WriteLine(b.GetType());
+            Console.WriteLine(b2.GetType());
+            Console.WriteLine(c.GetType());
+
+        }
+    }
+}
