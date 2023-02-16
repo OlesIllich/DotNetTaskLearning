8e2c8f3336e750b2a3a51a555e1a8685601bc92f
OlesIllich <116875760+OlesIllich@users.noreply.github.com>
2023-02-16 21:57:35 +0200

    add LINQ tasks

---
+++ DotNetLearning/LINQTasks/LINQTasks.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotNetLearning.LINQTasks
+{
+    public class LINQTestData
+    {
+        public int FirstValue { get; set; }
+
+        public int SecondValue { get; set; }
+
+        public DateTime DateTime { get; set; }
+    }
+
+    public class LINQTasks
+    {
+        public List<LINQTestData> OnlyPositiveFirstValue(List<LINQTestData> dataList)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int FirstValueSecondValueSum(List<LINQTestData> dataList)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool IsEmpty(List<LINQTestData> dataList)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<DateTime> ReturnDateTimeList(List<LINQTestData> dataList)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
